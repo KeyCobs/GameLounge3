db25b06011589ae93a111f793a6d6a3d34f1f845
KeyCobs <kevinjacobs@live.nl>
2022-03-08 22:44:24 +0100

    Init week 3 / Movement and gravity
    Gravity switch with secondes as fix and isGrounded

--- GameLounge/Assets/Assets/Scripts/Player/Movement.cs
+++ GameLounge/Assets/Assets/Scripts/Player/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,12 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     CharacterController charContr;
     [SerializeField]
-    float g_Gravity = 9.8f;
-    float g_Speed = 50;
-    float g_JumpSpeed = 65;
-
-     private Vector3 g_Move = new Vector3();
+    public float g_Gravity = 9.8f;
+    public float g_Speed = 5;
+    public float g_JumpSpeed = 10;
+    float g_Time = 0;
+    float g_TimeJump = 0;
+    private Vector3 g_Move = new Vector3();
     void Start()
     {
         charContr = GetComponent<CharacterController>();
@@ -27,49 +29,78 @@ public class Movement : MonoBehaviour
     void MovePlayer()
     {
 
-        //var hor = Input.GetAxis("Horzintal");
-        //var ver = Input.GetAxis("Vertical");
+        g_Move = StopMovingPlayer(g_Move);
+        g_Move = MovePlayer(g_Move);
+        g_Move.y -= g_Gravity * Time.deltaTime;
+        charContr.Move(g_Move * Time.deltaTime);
+    }
 
-        //transform.Translate(new Vector3(hor, g_Move.y, ver) * g_Speed * Time.deltaTime);
-
-        //if (charContr.isGrounded)
-        //{
-        //    g_Move = new Vector3(hor,0.0f,ver);
-        //    g_Move *= g_Speed;
-
-        //    if (Input.GetButton("Jump"))
-        //    {
-        //        g_Move.y = g_JumpSpeed;
-        //    }
-        //}
-
-        if (charContr.isGrounded)
+    private Vector3 MovePlayer(Vector3 move)
+    {
+        float setTime = 0.6f;
+        float setJumpTimer = 0.5f;
+        //switch
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                g_Move.x = g_Speed * Time.deltaTime;
-            }
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                g_Move.z = g_Speed * Time.deltaTime;
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                g_Move.z = -g_Speed * Time.deltaTime;
-            }
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                g_Move.x = -g_Speed * Time.deltaTime;
-            }
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                g_Move.y = g_JumpSpeed * Time.deltaTime;
-            }
+            move.x = g_Speed;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            move.z = g_Speed;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            move.z = -g_Speed;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            move.x = -g_Speed;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && charContr.isGrounded)
+        {
+            move.y = g_JumpSpeed;
+            g_TimeJump = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && g_TimeJump > setJumpTimer && g_Gravity < 0)
+        {
+            move.y = g_JumpSpeed;
+            g_TimeJump = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.F) && (g_Time > setTime))
+        {
+            Vector3 rot = new Vector3(180.0f,0.0f,0.0f) { };
+            g_Gravity = -g_Gravity;
+            g_JumpSpeed = -g_JumpSpeed;
+            move.y = 0;
+            g_Time = 0;
+            transform.Rotate(rot);
+            g_TimeJump = -0.5f;
         }
 
 
 
-        g_Move.y -= g_Gravity * Time.deltaTime;
-        charContr.Move(g_Move * Time.deltaTime);
+        g_Time += Time.deltaTime;
+        g_TimeJump += Time.deltaTime;
+        print(move.y);
+        return move;
+    }
+
+    private Vector3 StopMovingPlayer(Vector3 move)
+    {
+
+        bool stopHorizontal = ((Input.GetKeyUp(KeyCode.A) && !Input.GetKey(KeyCode.D)) ||( Input.GetKeyUp(KeyCode.D) && !Input.GetKey(KeyCode.A)));
+        if (stopHorizontal)
+        {
+            move.x = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            move.z = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            move.z = 0;
+        }
+        return move;
     }
 }
